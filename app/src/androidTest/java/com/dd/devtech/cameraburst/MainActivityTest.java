62038381b2ba5b05549fc192c6f8177c4dd9c297
Ritesh Kumar Sinha <01ritzs@gmail.com>
2021-07-08 02:58:40 +0530

    Addition of test cases

---
+++ app/src/androidTest/java/com/dd/devtech/cameraburst/MainActivityTest.java
@@ -0,0 +1,27 @@
+package com.dd.devtech.cameraburst;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testLaunch(){
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
